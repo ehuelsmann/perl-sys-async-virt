c3fde3da7ddc5092eaaf0d21082297e8f5805814
Erik Huelsmann <ehuels@gmail.com>
2024-09-14 15:51:05 +0200

    Add remaining callback entrypoints

--- eg/client.pl
+++ eg/client.pl
@@ -86,7 +86,7 @@ await $virt->open( 'qemu:///system' );
 
 use Data::Dumper;
 try {
-    my $es = await $virt->domain_event_callback_register_any(
+    my $es = await $virt->domain_event_register_any(
         $virt->DOMAIN_EVENT_ID_LIFECYCLE);
     my $rv = await $virt->list_all_domains();
     #say Dumper($rv);
