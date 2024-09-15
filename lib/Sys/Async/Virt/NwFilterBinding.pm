629e0b5b33bc5b7dde598d3e6b91e5de9afd16b4
Erik Huelsmann <ehuels@gmail.com>
2024-09-15 22:15:39 +0200

    Add license and copyright

--- lib/Sys/Async/Virt/NwFilterBinding.pm
+++ lib/Sys/Async/Virt/NwFilterBinding.pm
@@ -101,3 +101,8 @@ L<LibVirt|https://libvirt.org>, L<Sys::Virt>
 
 =head1 LICENSE AND COPYRIGHT
 
+
+  Copyright (C) 2024 Erik Huelsmann
+
+All rights reserved. This program is free software;
+you can redistribute it and/or modify it under the same terms as Perl itself.
